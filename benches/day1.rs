5fcdba655b6f13294e0cf0820be5700c868cb087
Bastian Asmussen <bastian@asmussen.tech>
2024-12-02 11:53:49 +0100

    feat: split Day1 into Day1Part1 and Day1Part2 for separate benchmarking and testing

--- benches/day1.rs
+++ benches/day1.rs
@@ -1,10 +1,14 @@
 use std::hint::black_box;
 
-use aoc::{day_1::Day1, Day};
+use aoc::{
+    day_1::{Day1Part1, Day1Part2},
+    Day,
+};
 use criterion::{criterion_group, criterion_main, Criterion};
 
 fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("day1 input", |b| b.iter(|| black_box(Day1::run)));
+    c.bench_function("day1part1", |b| b.iter(|| black_box(Day1Part1::run)));
+    c.bench_function("day1part2", |b| b.iter(|| black_box(Day1Part2::run)));
 }
 
 criterion_group!(benches, criterion_benchmark);
--- src/day_1/mod.rs
+++ src/day_1/mod.rs
@@ -1,13 +1,13 @@
 use crate::Day;
 
-pub struct Day1;
-impl Day for Day1 {
+const INPUT: &str = include_str!("input.txt");
+
+pub struct Day1Part1;
+impl Day for Day1Part1 {
     type Data = u32;
 
     fn run() -> Self::Data {
-        let input = include_str!("./input.txt");
-
-        Self::sanitize(input)
+        Self::sanitize(INPUT)
     }
 
     fn sanitize(input: &str) -> Self::Data {
@@ -33,14 +33,49 @@ impl Day for Day1 {
     }
 }
 
+pub struct Day1Part2;
+impl Day for Day1Part2 {
+    type Data = u32;
+
+    fn run() -> Self::Data {
+        Self::sanitize(INPUT)
+    }
+
+    fn sanitize(input: &str) -> Self::Data {
+        let mut left = Vec::new();
+        let mut right = Vec::new();
+
+        for (i, value) in input.split_whitespace().enumerate() {
+            let value: u32 = value.parse().expect("numeric value");
+            if i % 2 == 0 {
+                left.push(value);
+            } else {
+                right.push(value);
+            }
+        }
+
+        left.iter()
+            .map(|a| *a * right.iter().filter(|b| a == *b).count() as u32)
+            .sum()
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn check() {
+    fn check_part1() {
         let expected = 2_057_374;
-        let actual = Day1::run();
+        let actual = Day1Part1::run();
+
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn check_part2() {
+        let expected = 23_177_084;
+        let actual = Day1Part2::run();
 
         assert_eq!(expected, actual);
     }
